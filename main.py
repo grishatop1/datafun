2878ee345c4a57748edb986cdad05d5ea7751ca0
grisha <grigorije05@gmail.com>
2022-08-28 16:36:18 +0200

    progress

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from flask import Flask
-from flask import render_template, url_for
+from flask import render_template, url_for, request, jsonify
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_url_path="",
+    static_folder="static"
+)
 
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
 
+@app.route('/data')
+def data():
+    return jsonify({
+        'ip': request.remote_addr
+    })
+
 @app.route('/help')
 def help_page():
     return render_template("help.html")
+
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
---
+++ static/script.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", function() {
+    start();
+});
+
+let loading_text = document.getElementById("loading");
+let output_text = document.getElementById("output");
+
+async function typeWriter(txt) {
+    txt = String(txt)
+    if (!txt) {return;}
+    let i = 0;
+    while (i < txt.length) {
+      document.getElementById("output").innerHTML += txt.charAt(i);
+      i++;
+      await new Promise(resolve => setTimeout(resolve, 80))
+    }
+    document.getElementById("output").innerHTML += "<br>";
+  }
+
+var data = [
+    screen.width,
+    history.length,
+    decodeURIComponent(document.cookie.split(";")),
+    
+    screen.height,
+    document.width,
+    document.height,
+    innerWidth,
+    innerHeight,
+    screen.availWidth,
+    screen.availHeight,
+    screen.colorDepth,
+    screen.pixelDepth,
+];
+
+async function start() {
+    let audio = await new Promise(function(resolve, reject) {
+        let audio = new Audio("media/dubstep.mp3");
+        audio.addEventListener("canplay", ()=>{
+            resolve(audio);
+        });
+      });
+    let ip = await getIP();
+    audio.play()
+    await new Promise(resolve => setTimeout(resolve, 1800))
+    document.body.removeChild(loading_text);
+
+    await typeWriter(ip);
+    await typeWriter(platform.name); // 'IE'
+    await typeWriter(platform.version); // '10.0'
+    await typeWriter(platform.layout); // 'Trident'
+    await typeWriter(platform.description); // 'IE 10.0 x86 (platform preview; running in IE 7 mode) on Windows Server 2008 R2 / 7 x64'
+    await typeWriter(screen.width + "x" + screen.height);
+}
+
+async function getIP() {
+    let response = await fetch("/data");
+    let data = await response.json()
+    let ip = data["ip"]
+    return ip
+}
